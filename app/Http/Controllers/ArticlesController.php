3c367ca7498ac6dca596af927cbdd1b9628c5602
Mubashar Khokhar <m.khokhar@social-gizmo.com>
2023-05-20 00:29:24 +0200

    Body added to article by id endpoint

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -41,36 +41,13 @@ class ArticlesController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
      * Display the specified resource.
      */
-    public function show(int $id)
+    public function show(int $id, Request $request)
     {
+        $request->merge(['fields' => 'body']);
         return new ArticleResource(
             $this->service->findById($id)
         );
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
     }
 }
